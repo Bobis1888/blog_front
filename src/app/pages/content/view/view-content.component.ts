5224e0b0a14064fbc9cf4d80bdf3ee45f8f321f3
nelmin <bobis1888@gmail.com>
2024-07-31 19:55:35 +0500

    tag fix

--- src/app/pages/content/view/view-content.component.ts
+++ src/app/pages/content/view/view-content.component.ts
@@ -1,23 +1,26 @@
 import {Component, OnInit} from '@angular/core';
-import {ContentService, Filter, ListResponse, Status} from "src/app/core/service/content/content.service";
-import {ActivatedRoute, Router, RouterLink} from "@angular/router";
-import {delay, Observable, takeUntil} from "rxjs";
-import {UnSubscriber} from "src/app/core/abstract/un-subscriber";
-import {DeviceDetectorService} from "ngx-device-detector";
-import {ClipboardService} from "ngx-clipboard";
-import {MatSnackBar, MatSnackBarRef} from "@angular/material/snack-bar";
-import {TranslateModule} from "@ngx-translate/core";
-import {AuthService} from "app/core/service/auth/auth.service";
-import {NgxSkeletonLoaderModule} from "ngx-skeleton-loader";
-import {MaterialModule} from "app/theme/material/material.module";
 import {
-  CommonModule,
-} from "@angular/common";
-import {SafeHtmlPipe} from "app/core/pipe/safe-html";
-import {Meta} from "@angular/platform-browser";
-import {Content, Reaction} from "app/core/service/content/content";
-import {SubscriptionService} from "app/core/service/content/subscription.service";
-import {animations} from "app/core/config/app.animations";
+  ContentService,
+  Filter,
+  ListResponse,
+  Status,
+} from 'src/app/core/service/content/content.service';
+import {ActivatedRoute, Router, RouterLink} from '@angular/router';
+import {delay, Observable, takeUntil} from 'rxjs';
+import {UnSubscriber} from 'src/app/core/abstract/un-subscriber';
+import {DeviceDetectorService} from 'ngx-device-detector';
+import {ClipboardService} from 'ngx-clipboard';
+import {MatSnackBar, MatSnackBarRef} from '@angular/material/snack-bar';
+import {TranslateModule} from '@ngx-translate/core';
+import {AuthService} from 'app/core/service/auth/auth.service';
+import {NgxSkeletonLoaderModule} from 'ngx-skeleton-loader';
+import {MaterialModule} from 'app/theme/material/material.module';
+import {CommonModule} from '@angular/common';
+import {SafeHtmlPipe} from 'app/core/pipe/safe-html';
+import {Meta} from '@angular/platform-browser';
+import {Content, Reaction} from 'app/core/service/content/content';
+import {SubscriptionService} from 'app/core/service/content/subscription.service';
+import {animations} from 'app/core/config/app.animations';
 
 @Component({
   selector: 'view-content',
@@ -32,10 +35,9 @@ import {animations} from "app/core/config/app.animations";
   ],
   animations: animations,
   templateUrl: './view-content.component.html',
-  styleUrl: './view-content.component.less'
+  styleUrl: './view-content.component.less',
 })
 export class ViewContentComponent extends UnSubscriber implements OnInit {
-
   protected content: Content = {} as Content;
   protected authorContents: Array<Content> = [];
   protected tagContents: Array<Content> = [];
@@ -44,15 +46,17 @@ export class ViewContentComponent extends UnSubscriber implements OnInit {
   protected readonly Status = Status;
   private id: string = '';
 
-  constructor(protected contentService: ContentService,
-              protected deviceService: DeviceDetectorService,
-              protected matSnackBar: MatSnackBar,
-              protected meta: Meta,
-              protected subsService: SubscriptionService,
-              protected authService: AuthService,
-              protected clipboardService: ClipboardService,
-              protected router: Router,
-              protected aRouter: ActivatedRoute) {
+  constructor(
+    protected contentService: ContentService,
+    protected deviceService: DeviceDetectorService,
+    protected matSnackBar: MatSnackBar,
+    protected meta: Meta,
+    protected subsService: SubscriptionService,
+    protected authService: AuthService,
+    protected clipboardService: ClipboardService,
+    protected router: Router,
+    protected aRouter: ActivatedRoute,
+  ) {
     super();
   }
 
@@ -61,19 +65,24 @@ export class ViewContentComponent extends UnSubscriber implements OnInit {
   }
 
   get favoriteReaction(): Reaction {
-    return this.content.reactions.find(it => it.value === 'favorite') ?? {} as Reaction;
+    return (
+      this.content.reactions.find((it) => it.value === 'favorite') ??
+      ({} as Reaction)
+    );
   }
 
   get reactions(): Array<Reaction> {
-    return this.content.reactions.filter(it => it.value != 'favorite' && it.value != 'favorite_outline') ?? []
+    return (
+      this.content.reactions.filter(
+        (it) => it.value != 'favorite' && it.value != 'favorite_outline',
+      ) ?? []
+    );
   }
 
   ngOnInit(): void {
-
     this.aRouter.params
       .pipe(takeUntil(this.unSubscriber))
-      .subscribe(params => {
-
+      .subscribe((params) => {
         if (params['id'] != this.id) {
           this.id = params['id'];
           this.init(params['id']);
@@ -82,13 +91,14 @@ export class ViewContentComponent extends UnSubscriber implements OnInit {
   }
 
   share() {
-
     if (this.isMobile) {
-      navigator.share({
-        title: this.content.title,
-        text: this.translate.instant('viewContentPage.shareText'),
-        url: location.href
-      }).then();
+      navigator
+        .share({
+          title: this.content.title,
+          text: this.translate.instant('viewContentPage.shareText'),
+          url: location.href,
+        })
+        .then();
       return;
     }
 
@@ -96,46 +106,48 @@ export class ViewContentComponent extends UnSubscriber implements OnInit {
     this.ref = this.matSnackBar.open(
       this.translate.instant('viewContentPage.sharedSuccess'),
       undefined,
-      {duration: 3000, panelClass: 'snack-bar'});
+      {duration: 3000, panelClass: 'snack-bar'},
+    );
   }
 
   react(value: string = 'favorite') {
     let obs: Observable<any>;
 
-    if (this.content.reactions.find(it => it.value === value)?.isUserReaction) {
+    if (
+      this.content.reactions.find((it) => it.value === value)?.isUserReaction
+    ) {
       obs = this.contentService.removeReact(this.content.id);
     } else {
       obs = this.contentService.react(this.content.id, value);
     }
 
-    obs.pipe(takeUntil(this.unSubscriber))
-      .subscribe({
-        next: () => {
-
-          if (!this.content.reactions.find(it => it.value == value)) {
-            this.content.reactions.push({
-              value: value,
-              count: 0,
-              isUserReaction: false
-            });
-          }
-
-          this.content.reactions.map(it => {
-
-            if (it.value === value) {
-              it.count = it.isUserReaction ? it.count - 1 : it.count + 1;
-              it.isUserReaction = !it.isUserReaction;
-            } else {
-              if (it.isUserReaction) {
-                it.count = it.count - 1;
-                it.isUserReaction = false;
-              }
-            }
+    obs.pipe(takeUntil(this.unSubscriber)).subscribe({
+      next: () => {
+        if (!this.content.reactions.find((it) => it.value == value)) {
+          this.content.reactions.push({
+            value: value,
+            count: 0,
+            isUserReaction: false,
           });
-
-          this.content.reactions = this.content.reactions.filter(it => it.count > 0);
         }
-      });
+
+        this.content.reactions.map((it) => {
+          if (it.value === value) {
+            it.count = it.isUserReaction ? it.count - 1 : it.count + 1;
+            it.isUserReaction = !it.isUserReaction;
+          } else {
+            if (it.isUserReaction) {
+              it.count = it.count - 1;
+              it.isUserReaction = false;
+            }
+          }
+        });
+
+        this.content.reactions = this.content.reactions.filter(
+          (it) => it.count > 0,
+        );
+      },
+    });
   }
 
   bookmark() {
@@ -147,15 +159,19 @@ export class ViewContentComponent extends UnSubscriber implements OnInit {
       obs = this.contentService.saveToBookmarks(this.content.id);
     }
 
-    obs.pipe(takeUntil(this.unSubscriber))
-      .subscribe({
-        next: () => {
-          this.ref?.dismiss();
-          this.content.isSaved = !this.content.isSaved;
-          let message = this.translate.instant(`viewContentPage.${this.content.isSaved ? 'addedToBookmarks' : 'removedFromBookmarks'}`);
-          this.ref = this.matSnackBar.open(message, undefined, {duration: 3000, panelClass: 'snack-bar'});
-        }
-      });
+    obs.pipe(takeUntil(this.unSubscriber)).subscribe({
+      next: () => {
+        this.ref?.dismiss();
+        this.content.isSaved = !this.content.isSaved;
+        let message = this.translate.instant(
+          `viewContentPage.${this.content.isSaved ? 'addedToBookmarks' : 'removedFromBookmarks'}`,
+        );
+        this.ref = this.matSnackBar.open(message, undefined, {
+          duration: 3000,
+          panelClass: 'snack-bar',
+        });
+      },
+    });
   }
 
   override ngOnDestroy() {
@@ -165,13 +181,11 @@ export class ViewContentComponent extends UnSubscriber implements OnInit {
 
   init(id: string) {
     this.state = 'loading';
-    this.contentService.get(id)
-      .pipe(
-        takeUntil(this.unSubscriber),
-        delay(300),
-      )
+    this.contentService
+      .get(id)
+      .pipe(takeUntil(this.unSubscriber), delay(300))
       .subscribe({
-        next: it => {
+        next: (it) => {
           this.content = it;
           this.state = 'data';
 
@@ -181,73 +195,85 @@ export class ViewContentComponent extends UnSubscriber implements OnInit {
 
           if (this.content.tags) {
             this.meta.updateTag({
-              name: 'keywords', content: this.content.tags
-                .map((it) => it.replace('#', '')).join(' ')
+              name: 'keywords',
+              content: this.content.tags
+                .map((it) => it.replace('#', ''))
+                .join(' '),
             });
           }
 
           this.listByParams(this.content.authorName)
             .pipe(takeUntil(this.unSubscriber))
             .subscribe({
-              next: (it) => this.authorContents = it?.list ?? []
+              next: (it) => (this.authorContents = it?.list ?? []),
             });
-          this.listByParams('', this.content.tags)
-            .pipe(takeUntil(this.unSubscriber))
-            .subscribe({
-              next: (it) => this.tagContents = it?.list ?? []
-            });
+
+          if (this.content.tags?.length > 0) {
+            this.listByParams('', this.content.tags)
+              .pipe(takeUntil(this.unSubscriber))
+              .subscribe({
+                next: (it) => (this.tagContents = it?.list ?? []),
+              });
+          }
         },
-        error: () => this.state = 'empty'
+        error: () => (this.state = 'empty'),
       });
   }
 
   unsubscribe() {
-    this.subsService.unsubscribe(this.content.authorName)
+    this.subsService
+      .unsubscribe(this.content.authorName)
       .pipe(takeUntil(this.unSubscriber))
       .subscribe({
         next: (res) => {
-
           if (res.success) {
             this.content.actions.canUnsubscribe = false;
             this.content.actions.canSubscribe = true;
           }
-        }
+        },
       });
   }
 
   subscribe() {
-    this.subsService.subscribe(this.content.authorName)
+    this.subsService
+      .subscribe(this.content.authorName)
       .pipe(takeUntil(this.unSubscriber))
       .subscribe({
         next: (res) => {
-
           if (res.success) {
             this.content.actions.canUnsubscribe = true;
             this.content.actions.canSubscribe = false;
           }
-        }
+        },
       });
   }
 
-  listByParams(author: string = '', tags: string[] = []): Observable<ListResponse> {
+  listByParams(
+    author: string = '',
+    tags: string[] = [],
+  ): Observable<ListResponse> {
     return this.contentService.list({
       max: 3,
       page: 0,
       search: {
         author: author,
-        tags: tags
+        tags: tags,
       },
-      sortBy: ['countViews', 'date']
+      sortBy: ['countViews', 'date'],
     } as Filter);
   }
 
-  scrollToElement($element: any, behavior: string = "smooth"): void {
-    $element?.scrollIntoView({behavior: behavior, block: "start", inline: "nearest"});
+  scrollToElement($element: any, behavior: string = 'smooth'): void {
+    $element?.scrollIntoView({
+      behavior: behavior,
+      block: 'start',
+      inline: 'nearest',
+    });
   }
 
   nav(cont: Content) {
     this.state = this.id == cont.id ? 'data' : 'loading';
-    this.scrollToElement(document.getElementById("header"), 'auto');
+    this.scrollToElement(document.getElementById('header'), 'auto');
     this.router.navigate(['content', 'view', cont.id]).then();
   }
 }
